57513feb57fd8572c08bbbe0f1b941c3da8d8d97
Rahim_Badsa <rahimbadsa723@gmail.com>
2025-12-16 10:56:27 +0600

    advisor row created

---
+++ src/app/advisors/page.tsx
@@ -0,0 +1,21 @@
+import PageHero from '@/components/shared/PageHero';
+import Footer from '@/components/layout/Footer';
+import AdvisorsSection from '@/components/AdvisorsSection';
+import heroImg from '@assets/bg-img/advisors-banner.png';
+
+export default function AdvisorsPage() {
+  return (
+    <div>
+      <PageHero
+        title="Advisors"
+        subtitle="Lorem ipsum dolor sit amet consectetur. Neque aliquam amet commodo sollicitudin"
+        backgroundImage={heroImg}
+        height="compact"
+      />
+
+      <AdvisorsSection />
+
+      <Footer />
+    </div>
+  );
+}
