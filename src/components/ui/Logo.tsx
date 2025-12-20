3a0c757d8f2ff91cb7e55c9b614b18ddf3e03647
Rahim_Badsa <rahimbadsa723@gmail.com>
2025-12-20 10:53:05 +0600

    dashboard created

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import logo from '../../assets/logo/logo.png';
+import Link from 'next/link';
 
 export function Logo({style}: {style?: string}) {
-
    return (
-      <div  className={`relative  overflow-hidden rounded-full  flex items-center justify-center `}>
+      <Link
+         href='/'
+         className={`relative  overflow-hidden rounded-full  flex items-center justify-center `}>
          <Image
             src={logo}
             alt='Association logo'
             className={`object-cover ${style}`}
          />
-      </div>
+      </Link>
    );
 }
